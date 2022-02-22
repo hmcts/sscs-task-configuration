9f1d28e0309b4cd532765c1366b1cf1295dc997b
Benson Li <benson.li@evoco.co.uk>
2022-02-22 09:38:49 +0000

    SSCS-9992_Create_Review_Non_Compliant_Appeal_Task

---
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/draftToNonCompliant/CreateReviewDraftToNonCompliantAppealTest.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.reform.sscstaskconfiguration.dmn.draftToNonCompliant;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTable.WA_TASK_INITIATION_SSCS_BENEFIT;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTableBaseUnitTest;
+
+
+
+
+
+class CreateReviewDraftToNonCompliantAppealTest extends DmnDecisionTableBaseUnitTest {
+
+    @BeforeAll
+    public static void initialization() {
+        CURRENT_DMN_DECISION_TABLE = WA_TASK_INITIATION_SSCS_BENEFIT;
+    }
+
+    static Stream<Arguments> scenarioProvider() {
+
+        return Stream.of(
+            Arguments.of(
+                "draftToNonCompliant",
+                singletonList(
+                    Map.of(
+                        "taskId", "nonCompliantCase",
+                        "name", "Review non-compliant appeal",
+                        "group", "TCW",
+                        "processCategories", "Non-compliant appeal"
+
+                    )
+                )
+            )
+        );
+    }
+
+    @ParameterizedTest(name = "event id: {0} pre event state: {1}  post event state: {2} appeal type: {3}")
+    @MethodSource("scenarioProvider")
+    void given_multiple_event_ids_should_evaluate_dmn(String eventId,
+                                                      List<Map<String, String>> expectation) {
+
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("eventId", eventId);
+
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        assertThat(dmnDecisionTableResult.getResultList(), is(expectation));
+
+    }
+}
