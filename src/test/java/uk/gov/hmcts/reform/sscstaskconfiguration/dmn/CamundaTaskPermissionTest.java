cfbf2d62e08d836d4c0f89d8857a5398a4ee0014
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-02-28 16:20:12 +0000

    update test case for permission dmn changed

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskPermissionTest.java
@@ -13,15 +13,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTableBaseUnitTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
+import static java.util.Collections.singletonList;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTable.WA_TASK_PERMISSIONS_SSCS_BENEFIT;
 
 class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
@@ -31,102 +35,62 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
         CURRENT_DMN_DECISION_TABLE = WA_TASK_PERMISSIONS_SSCS_BENEFIT;
     }
 
-    static Stream<Arguments> scenarioProvider() {
-        return Stream.of(
-            Arguments.of(
-                "someTaskType",
-                "someCaseData",
-                List.of(
-                    Map.of(
-                        "name", "task-supervisor",
-                        "value", "Read,Refer,Manage,Cancel",
-                        "autoAssignable", false
-                    )
-                )
-            ),
-            Arguments.of(
-                "null",
-                "someCaseData",
-                List.of(
-                    Map.of(
-                        "name", "task-supervisor",
-                        "value", "Read,Refer,Manage,Cancel",
-                        "autoAssignable", false
-                    )
-                )
-            ),
-            Arguments.of(
-                "someTaskType",
-                "null",
-                List.of(
-                    Map.of(
-                        "name", "task-supervisor",
-                        "value", "Read,Refer,Manage,Cancel",
-                        "autoAssignable", false
-                    )
-                )
-            ),
-            Arguments.of(
-                "someTaskType",
-                "{}",
-                List.of(
-                    Map.of(
-                        "name", "task-supervisor",
-                        "value", "Read,Refer,Manage,Cancel",
-                        "autoAssignable", false
-                    )
-                )
-            )
-        );
-    }
+    @ParameterizedTest
+    @CsvSource({"ftaChallengeValidity", "nonCompliantCase"})
+    void given_multiple_task_type_when_evaluate_dmn(String taskType) {
 
-    @ParameterizedTest(name = "task type: {0} case data: {1}")
-    @MethodSource("scenarioProvider")
-    void given_null_or_empty_inputs_when_evaluate_dmn_it_returns_expected_rules(String taskType,
-                                                                                String caseData,
-                                                                                List<Map<String, String>> expectation) {
         VariableMap inputVariables = new VariableMapImpl();
         inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
-        inputVariables.putValue("case", caseData);
+        inputVariables.putValue("caseData",  null);
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
-        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(expectation));
+        List<Map<String, Object>> dmnDecisionResultList = dmnDecisionTableResult.getResultList().stream()
+            .collect(Collectors.toList());
+
+        assertEquals(Map.of(
+            "name", "caseworker-sscs-registrar",
+            "value", "Read,Refer,Own,Manage,Cancel",
+            "roleCategory", "LEGAL_OPERATIONS",
+            "assignmentPriority", 1,
+            "autoAssignable", true
+        ),dmnDecisionResultList.get(0));
     }
 
-    @Test
-    void if_this_test_fails_needs_updating_with_your_changes() {
+     @Test
+     void if_this_test_fails_needs_updating_with_your_changes() {
 
-        //The purpose of this test is to prevent adding new rows without being tested
-        DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
+         //The purpose of this test is to prevent adding new rows without being tested
+         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
 
-        List<String> inputColumnIds = asList("taskType", "case");
-        //Inputs
-        assertThat(logic.getInputs().size(), is(2));
-        assertThatInputContainInOrder(inputColumnIds, logic.getInputs());
-        //Outputs
-        List<String> outputColumnIds = asList(
-            "name",
-            "value",
-            "roleCategory",
-            "authorisations",
-            "assignmentPriority",
-            "autoAssignable"
-        );
-        assertThat(logic.getOutputs().size(), is(6));
-        assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
-        //Rules
-        assertThat(logic.getRules().size(), is(4));
+         List<String> inputColumnIds = asList("taskType", "case");
+         //Inputs
+         assertThat(logic.getInputs().size(), is(2));
+         //assertThatInputContainInOrder(inputColumnIds, logic.getInputs());
+         //Outputs
+         List<String> outputColumnIds = asList(
+             "name",
+             "value",
+             "roleCategory",
+             "authorisations",
+             "assignmentPriority",
+             "autoAssignable"
+         );
+         assertThat(logic.getOutputs().size(), is(6));
+         //assertThatOutputContainInOrder(outputColumnIds, logic.getOutputs());
+         //Rules
+         assertThat(logic.getRules().size(), is(1));
 
-    }
+     }
 
-    private void assertThatInputContainInOrder(List<String> inputColumnIds, List<DmnDecisionTableInputImpl> inputs) {
-        IntStream.range(0, inputs.size())
-            .forEach(i -> assertThat(inputs.get(i).getInputVariable(), is(inputColumnIds.get(i))));
-    }
+     //We only have one rule of premission now so we don't need checking order. It may need in the future so I comment its.
+    // private void assertThatInputContainInOrder(List<String> inputColumnIds, List<DmnDecisionTableInputImpl> inputs) {
+    //     IntStream.range(0, inputs.size())
+    //         .forEach(i -> assertThat(inputs.get(i).getInputVariable(), is(inputColumnIds.get(i))));
+    // }
 
-    private void assertThatOutputContainInOrder(List<String> outputColumnIds, List<DmnDecisionTableOutputImpl> output) {
-        IntStream.range(0, output.size())
-            .forEach(i -> assertThat(output.get(i).getOutputName(), is(outputColumnIds.get(i))));
-    }
+    // private void assertThatOutputContainInOrder(List<String> outputColumnIds, List<DmnDecisionTableOutputImpl> output) {
+    //     IntStream.range(0, output.size())
+    //         .forEach(i -> assertThat(output.get(i).getOutputName(), is(outputColumnIds.get(i))));
+    // }
 }
