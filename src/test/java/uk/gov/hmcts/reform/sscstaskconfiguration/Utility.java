7a23946608bfea9c5396190c6aa4c3cbe242ed08
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-03-03 16:15:54 +0000

    Create a Utility class for CamundaTaskPermissionTest use in the future

---
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/Utility.java
@@ -0,0 +1,11 @@
+public class Utility{
+    private void assertThatInputContainInOrder(List<String> inputColumnIds, List<DmnDecisionTableInputImpl> inputs) {
+        IntStream.range(0, inputs.size())
+            .forEach(i -> assertThat(inputs.get(i).getInputVariable(), is(inputColumnIds.get(i))));
+    }
+
+    private void assertThatOutputContainInOrder(List<String> outputColumnIds, List<DmnDecisionTableOutputImpl> output) {
+        IntStream.range(0, output.size())
+            .forEach(i -> assertThat(output.get(i).getOutputName(), is(outputColumnIds.get(i))));
+    }
+}
