df94e01982cd86556ac028e619821bab19826801
Mark Brooks <mark.brooks@hmcts.net>
2023-03-17 16:08:49 +0000

    Updated unit tests

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CaumundaTaskTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CaumundaTaskTypeTest.java
@@ -4,27 +4,32 @@ import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTable;
 import uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTableBaseUnitTest;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
 
 class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
 
     @BeforeAll
     public static void initialization() {
         CURRENT_DMN_DECISION_TABLE = DmnDecisionTable.WA_TASK_TYPES_SSCS_BENEFIT;
+    }
+
+    @Test
+    void task_type_entries_have_correct_format() {
+        VariableMap inputVariables = new VariableMapImpl();
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+        for(Map<String,Object> taskType : dmnDecisionTableResult.getResultList()) {
+            assertThat((String) taskType.get("taskTypeId"), matchesPattern("^[a-z][A-Za-z0-9]*$"));
+            assertThat((String) taskType.get("taskTypeName"), matchesPattern("[A-Z]*$|^[A-Z][A-Za-z \\-]*[A-Za-z]$"));
+        }
     }
 
     @Test
