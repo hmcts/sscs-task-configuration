47cf3a56730b11439746e0d4dfa9730f34aea811
Mark Brooks <mark.brooks@hmcts.net>
2023-04-04 09:29:34 +0100

    Added task completion and cancellation

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -39,6 +39,15 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "completionMode", "Auto"
                     )
                 )
+            ),
+            Arguments.of(
+                "requestInfoFromParty",
+                asList(
+                    Map.of(
+                        "taskType", "reviewIncompleteAppeal",
+                        "completionMode", "Auto"
+                    )
+                )
             )
         );
     }
@@ -59,7 +68,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(1));
+        assertThat(logic.getRules().size(), is(2));
 
     }
 
