87eebb012e42aeb01516d928c62fc1c869fc328f
Jephtah Addison <jephtah.addison@hmcts.net>
2023-05-09 11:17:17 +0100

    final commit for all unit tests passing

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskPermissionTest.java
@@ -64,19 +64,34 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
                 "someCaseData",
                 List.of(
                     Map.of(
+                        "name", "case-allocator",
+                        "value", "Read,Own,Manage,Complete,Cancel,Assign,Unassign,Claim,Unclaim",
+                        "autoAssignable", false
+                    ),
+                    Map.of(
+                        "name", "task-supervisor",
+                        "value", "Read,Own,Manage,Complete,Cancel,Assign,Unassign,Claim,Unclaim",
+                        "autoAssignable", false
+                    ),
+                    Map.of(
                         "name", "Allocated-CTSC-Caseworker",
                         "value", "Read,Own,Claim,Unclaim,Manage,Cancel,UnclaimAssign,CompleteOwn",
+                        "assignmentPriority", 1,
+                        "roleCategory", "CTSC",
                         "autoAssignable", true
                     ),
                     Map.of(
                         "name", "CTSC-Administrator",
                         "value", "Read,Own,Claim,Unclaim,Manage,Cancel,UnclaimAssign,CompleteOwn",
-
+                        "assignmentPriority", 2,
+                        "roleCategory", "CTSC",
                         "autoAssignable", false
                     ),
                     Map.of(
                         "name", "CTSC-Team-Leader",
                         "value", "Read,Own,Claim,Unclaim,Manage,UnclaimAssign,Assign,Unassign,Cancel,CompleteOwn",
+                        "assignmentPriority", 3,
+                        "roleCategory", "CTSC",
                         "autoAssignable", false
                     )
                 )
