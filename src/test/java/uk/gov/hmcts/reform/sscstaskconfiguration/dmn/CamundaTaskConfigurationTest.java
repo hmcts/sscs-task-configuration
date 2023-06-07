14027f3d94433c96d25d8e2cfeaf53920df1ef37
paul-pearson <paul.pearson@version1.com>
2023-06-07 16:32:36 +0100

    SSCS-10005 Correct roleCategory

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -252,7 +252,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                         + "[Ability to amend interloc review state flag]"
                         + "(/case/SSCS/Benefit/${[CASE_REFERENCE]}/trigger/interlocReviewStateAmend)", true)
                     .expectedValue(WORK_TYPE, "pre_hearing", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY, "LEGAL_OPERATIONS", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             )
@@ -282,7 +282,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(34));
+        assertThat(logic.getRules().size(), is(35));
     }
 
     private void resultsMatch(List<Map<String, Object>> results, List<Map<String, Object>> expectation) {
