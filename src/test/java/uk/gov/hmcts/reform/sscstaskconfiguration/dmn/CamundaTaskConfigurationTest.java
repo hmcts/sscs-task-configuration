68426eadb0fc986f301a4e37d116e8046c6a094b
paul-pearson <paul.pearson@version1.com>
2023-06-08 09:10:27 +0100

    SSCS-10421 Fix rule count in test

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -300,7 +300,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(35));
+        assertThat(logic.getRules().size(), is(36));
     }
 
     private void resultsMatch(List<Map<String, Object>> results, List<Map<String, Object>> expectation) {
