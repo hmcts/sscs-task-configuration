e74b6227b28a88f30b6479a58da16d1368a70a41
markbrooks725 <mark.brooks@hmcts.net>
2023-09-08 10:59:35 +0100

    Revert "Updating DMN files with unit tests"
    This reverts commit 89a9d1b09f7da40efff963c120ed03348f9d1cc0.

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -437,19 +437,6 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
                     )
                 )
             ),
-            Arguments.of(
-                "permissionToAppealRequest",
-                "appealDormant",
-                null,
-                singletonList(
-                    Map.of(
-                        "taskId", "reviewApplicationAndAllocateJudge",
-                        "name", "Review Application And Allocate Judge",
-                        "workingDaysAllowed", 0,
-                        "processCategories", "reviewApplicationAndAllocateJudge"
-                    )
-                )
-            ),
             event("actionPostponementRequest")
                 .withCaseData("daysToHearing", 14)
                 .build(),
@@ -481,7 +468,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(17));
+        assertThat(logic.getRules().size(), is(16));
     }
 
     static Stream<Arguments> scenarioProviderDateDefaults() {
