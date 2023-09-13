a85f5658b095b2cd03f536ce8eec2066b09cdc61
paul-pearson <paul.pearson@version1.com>
2023-09-13 16:43:32 +0100

    SSCS-11294 Fix merging errors

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -67,11 +67,11 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(14));
+        assertThat(logic.getRules().size(), is(15));
     }
 
     public static Arguments eventAutoCompletesTasks(String event, String... tasks) {
-        return Arguments.of(event, Arrays.stream(tasks).map(t -> outputMap(t)).collect(Collectors.toList())
+        return Arguments.of(event, null, Arrays.stream(tasks).map(t -> outputMap(t)).collect(Collectors.toList())
         );
     }
 
