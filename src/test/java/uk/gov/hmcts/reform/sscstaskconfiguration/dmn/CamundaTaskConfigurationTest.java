b2041d04c9ef0b179cacfa07c7eed6225c865fd4
Mark Brooks <mark.brooks@hmcts.net>
2023-09-22 12:36:19 +0100

    Added region property to tasks, fixed capitalisation issue with roleCategory

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -243,7 +243,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                     .expectedValue(MINOR_PRIORITY, "500", true)
                     .expectedValue(MAJOR_PRIORITY, "5000", true)
                     .expectedValue(WORK_TYPE, "pre_hearing", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY, "JUDICIAL", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -254,7 +254,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                     .expectedValue(MINOR_PRIORITY, "100", true)
                     .expectedValue(MAJOR_PRIORITY, "1000", true)
                     .expectedValue(WORK_TYPE, "pre_hearing", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY,"JUDICIAL", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -265,7 +265,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                         .expectedValue(MINOR_PRIORITY, "500", true)
                         .expectedValue(MAJOR_PRIORITY, "5000", true)
                         .expectedValue(WORK_TYPE, "pre_hearing", true)
-                        .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                        .expectedValue(ROLE_CATEGORY,"JUDICIAL", true)
                         .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                         .build()
             ),
@@ -299,7 +299,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                         .expectedValue(MINOR_PRIORITY, "500", true)
                         .expectedValue(MAJOR_PRIORITY, "5000", true)
                         .expectedValue(WORK_TYPE, "hearing_work", true)
-                        .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                        .expectedValue(ROLE_CATEGORY, "JUDICIAL", true)
                         .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                         .build()
                 ),
@@ -355,7 +355,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                     .expectedValue(MINOR_PRIORITY, "100", true)
                     .expectedValue(MAJOR_PRIORITY, "1000", true)
                     .expectedValue(WORK_TYPE, "hearing_work", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY, "JUDICIAL", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -389,7 +389,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                     .expectedValue(MINOR_PRIORITY, "500", true)
                     .expectedValue(MAJOR_PRIORITY, "5000", true)
                     .expectedValue(WORK_TYPE, "pre_hearing", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY, "JUDICIAL", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -400,7 +400,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                     .expectedValue(MINOR_PRIORITY, "500", true)
                     .expectedValue(MAJOR_PRIORITY, "5000", true)
                     .expectedValue(WORK_TYPE, "pre_hearing", true)
-                    .expectedValue(ROLE_CATEGORY, "Judicial", true)
+                    .expectedValue(ROLE_CATEGORY, "JUDICIAL", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -446,7 +446,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(40));
+        assertThat(logic.getRules().size(), is(41));
     }
 
     private void resultsMatch(List<Map<String, Object>> results, List<Map<String, Object>> expectation) {
