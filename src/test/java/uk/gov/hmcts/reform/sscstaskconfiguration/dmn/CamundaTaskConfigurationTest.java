03c5c046658926f0d8a68bbe13b9ecbf367c286e
Mark Brooks <mark.brooks@hmcts.net>
2023-12-04 21:11:52 +0000

    Remove description from reviewStatementofReasonsApplication task

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -611,9 +611,6 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
                 CaseDataBuilder.defaultCase().build(),
                 ConfigurationExpectationBuilder.defaultExpectationsPostHearings()
                     .expectedValue("workType", "post_hearing", true)
-                    .expectedValue(DESCRIPTION, "[Send to Interloc - Review statement of reasons application]"
-                        + "(/case/SSCS/Benefit/${[CASE_REFERENCE]}"
-                        + "/trigger/validSendToInterloc)", true)
                     .expectedValue(DUE_DATE_INTERVAL_DAYS, "2", true)
                     .build()
             ),
@@ -805,7 +802,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(59));
+        assertThat(logic.getRules().size(), is(58));
     }
 
     private void resultsMatch(List<Map<String, Object>> results, List<Map<String, Object>> expectation) {
