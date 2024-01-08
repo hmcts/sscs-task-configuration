db9c5f33095f95936da6e995da8e837ba076696b
Nicholas Hill <nicholas.hill@HMCTS.net>
2024-01-08 11:20:46 +0000

    cleanup

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -856,7 +856,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(83));
+        assertThat(logic.getRules().size(), is(82));
     }
 
     static Stream<Arguments> scenarioProviderDateDefaults() {
