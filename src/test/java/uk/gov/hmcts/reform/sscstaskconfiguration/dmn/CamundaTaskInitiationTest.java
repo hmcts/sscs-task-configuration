369b18f1cdcfd1a3213ba25bdf7882909d90fa46
Nicholas Hill <nicholas.hill@HMCTS.net>
2024-01-09 15:05:50 +0000

    reset task init SSCS-12282

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -843,7 +843,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(77));
+        assertThat(logic.getRules().size(), is(76));
     }
 
     static Stream<Arguments> scenarioProviderDateDefaults() {
