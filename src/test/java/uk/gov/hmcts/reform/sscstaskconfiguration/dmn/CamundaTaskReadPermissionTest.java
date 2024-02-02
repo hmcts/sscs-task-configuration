6b55c28fc0e460e43d1df83e7463c46ecc102cb5
Mark Brooks <mark.brooks@hmcts.net>
2024-02-02 15:58:59 +0000

    Fix checkstyle issues

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskReadPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskReadPermissionTest.java
@@ -3,26 +3,20 @@ package uk.gov.hmcts.reform.sscstaskconfiguration.dmn;
 import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTableBaseUnitTest;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
 import static uk.gov.hmcts.reform.sscstaskconfiguration.DmnDecisionTable.WA_TASK_PERMISSIONS_SSCS_BENEFIT;
 
 public class CamundaTaskReadPermissionTest extends DmnDecisionTableBaseUnitTest {
 
-    private final String[] ROLES = {
+    private static final String[] ROLES = {
         "allocated-tribunal-caseworker",
         "tribunal-caseworker",
         "allocated-ctsc-caseworker",
@@ -65,17 +59,18 @@ public class CamundaTaskReadPermissionTest extends DmnDecisionTableBaseUnitTest 
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
-        for(String role : ROLES) {
-            List<Map<String,Object>> rules = dmnDecisionTableResult.getResultList().stream().filter(r -> role.equals(r.get("name"))).collect(Collectors.toList());
-            if(rules.isEmpty()) {
+        for (String role : ROLES) {
+            List<Map<String,Object>> rules = dmnDecisionTableResult.getResultList().stream()
+                .filter(r -> role.equals(r.get("name"))).collect(Collectors.toList());
+            if (rules.isEmpty()) {
                 System.out.println(String.format("Task %s, no read access for role %s", taskType, role));
                 hasReadAccess = false;
-            } else if(rules.size()>1) {
+            } else if (rules.size() > 1) {
                 System.out.println(String.format("Task %s, multiple entries for role %s", taskType, role));
                 hasReadAccess = false;
             } else {
                 String permissions = rules.get(0).get("value").toString();
-                if(!permissions.contains("Read")) {
+                if (!permissions.contains("Read")) {
                     System.out.println(String.format("Task %s, no read access for role %s", taskType, role));
                     hasReadAccess = false;
                 }
