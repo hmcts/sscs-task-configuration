96edf08c6554c2bf27558d69254b1cddf854bab2
Gemma Talbot <gemma.talbot@solirius.com>
2026-02-05 14:33:54 +0000

    add task completion

--- src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscstaskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -49,6 +49,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                                     "referredByAdminJudgePreHearing", "referredToInterlocJudge", "reviewBfDate",
                                     "referredByAdminJudgePostHearing", "referredByTcwPostHearing", BLANK),
             eventAutoCompletesTasks("hmctsResponseReviewed","reviewFtaResponse", BLANK),
+            eventAutoCompletesTasks("requestTranslationFromWLU","reviewBilingualDocument", BLANK),
             eventAutoCompletesTasks("reviewConfidentialityRequest","reviewConfidentialityRequest", BLANK),
             eventAutoCompletesTasks("reviewPhmeRequest","reviewPheRequestJudge", BLANK),
             eventAutoCompletesTasks("decisionIssued",
@@ -144,7 +145,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(46));
+        assertThat(logic.getRules().size(), is(47));
     }
 
     public static Arguments eventAutoCompletesTasks(String event, String... tasks) {
